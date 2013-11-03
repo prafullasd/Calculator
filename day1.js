29c2fcd06134fbb177a9637aa0dbebd00f9f8016
Prafulla <prafullasd@hotmail.com>
2013-11-03 17:21:15 -0500

    used class

---
+++ day1.js
@@ -0,0 +1,21 @@
+function clickHandler(index) {
+    var whatsInside = $("#text"+index).val(); //$ creates a jquery, # is for id , . for a class
+    var whatsInsideUp = whatsInside.toUpperCase() 
+    $("#text_out"+index).text(whatsInsideUp)
+    console.log(whatsInsideUp);
+}
+
+$(document).ready( function() {
+    $(".calculator").each( function() {
+    var inputBox=$('<input type="text" size="50">')
+    var calcButton=$('<p><button>Calculate</button></p>')
+    var output=$('<div id="text_out">')
+    calcButton.on("click", function(){
+        var whatsInside=inputBox.val()
+        output.text(whatsInside.toUpperCase())
+    }) //binding an event to  button
+    $(this).append(inputBox);
+    $(this).append(calcButton);
+    $(this).append(output);
+        })
+});
